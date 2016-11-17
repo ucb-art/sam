bc4ac8745c191a55f53dcf5c5b902c0b39e763f0
Howard Mao <zhehao.mao@gmail.com>
2016-11-16 21:07:26 -0800

    WIP changes

--- src/main/scala/SAM.scala
+++ src/main/scala/SAM.scala
@@ -15,43 +15,37 @@ case class SAMConfig(subpackets: Int, bufferDepth: Int = 2) {
 }
 
 // stream to axi4 memory io
-class SAMIO[T<:Data:Real](genIn: => T, val config: SAMConfig = SAMConfig()) extends Bundle {
+class SAMIO[T<:Data:Real](genIn: => T, val config: SAMConfig = SAMConfig())
+                         (implicit p: Parameters) extends ParameterizedBundle()(p) {
 
   val in = Input(ValidWithSync(genIn))
   val axi = Output(new NastiIO)
 }
 
 // stream to axi4 memory
-class SAM[T<:Data:Real](genIn: => T, val config: SAMConfig = SAMConfig()) extends Module {
+class SAM[T<:Data:Real](w: Int, val config: SAMConfig = SAMConfig())
+                       (implicit p: Parameters) extends NastiModule()(p) {
 
-  val io = IO(new SAMIO(genIn, config))
+  val io = IO(new SAMIO(UInt(width = w), config))
 
   // state machine stuff
   val sIdle :: sTriggered :: sWriting :: sReading :: Nil = Enum(UInt(), 4)
-  val stateWidth = log2Up(4)
-  val state = Reg(UInt(width=stateWidth), init=sIdle)
+  val state = Reg(init = sIdle)
 
   // memories
-  val write_indices = log2Up(config.subpackets*config.bufferDepth)
+  val write_indices = log2Ceil(config.subpackets*config.bufferDepth)
   val write_index = Counter(state === sWriting && io.in.valid, write_indices)._1
-  val mem = SeqMem(config.subpackets*config.bufferDepth, genIn)
+  val mem = SeqMem(config.subpackets*config.bufferDepth, UInt(width = w))
   
-  // axi reading stuff
-  // TODO: what are these variables? 
-  val ar = Queue(io.axi.ar, 1)
-  val read_index = ar.bits.addr(4, 3)
-  io.axi.r.valid := ar.valid
-  ar.ready := io.axi.r.ready
-
   // le state machine
   switch(state) {
-    is sIdle {
+    is (sIdle) {
      
     }
-    is sTriggered {
+    is (sTriggered) {
       when (io.in.sync && io.in.valid) { state := sWriting }
     }
-    is sWriting {
+    is (sWriting) {
       when (io.in.valid) {
         mem(write_index) := io.in.bits
         when (write_index === UInt(write_indices-1)) {
@@ -59,8 +53,67 @@ class SAM[T<:Data:Real](genIn: => T, val config: SAMConfig = SAMConfig()) extend
         }
       }
     }
-    is sReading {
+    is (sReading) {
     }
   }
-}
 
+  val rIdle :: rWait :: rReadFirst :: rSend :: Nil = Enum(Bits(), 3)
+  val rState = Reg(init = rIdle)
+  val rAddr = Reg(UInt(width = nastiXAddrBits - log2Ceil(w/8)))
+  val rLen = Reg(UInt(width = nastiXLenBits - log2Ceil(w/nastiXDataBits)))
+  val rawData = mem.read(rAddr)
+  val rData = Reg(UInt(width = w))
+  val rCount =
+    if (w == nastiXDataBits) Reg(UInt(width = 1))
+    else Reg(UInt(width = log2Ceil(w/nastiXDataBits)))
+  val rId = Reg(io.ar.bits.id)
+
+  when (io.ar.fire()) {
+    rAddr := io.ar.bits.addr >> UInt(log2Ceil(w/8))
+    rLen  := io.ar.bits.len  >> UInt(log2Ceil(w/nastiXDataBits))
+    rState := rWait
+  }
+
+  when (state === rWait) { rState := rRead }
+  when (state === rReadFirst) {
+    rData := rawData
+    rAddr := rAddr + UInt(1)
+    rCount := UInt(w/nastiXDataBits-1)
+    rId := io.ar.bits.id
+    rState := rSend
+  }
+  when (io.r.fire()) {
+    when (rCount === UInt(0)) {
+      when (rLen === UInt(0)) {
+        rState := rIdle
+      } .otherwise {
+        rData := rawData
+        rAddr := rAddr + UInt(1)
+        rLen := rLen - UInt(1)
+        rCount := UInt(w/nastiXDataBits-1)
+      }
+    } .otherwise {
+      rData := rData >> UInt(nastiXDataBits)
+      rCount := rCount - UInt(1)
+    }
+  }
+
+  io.ar.ready := (rState === rIdle)
+  io.r.valid := (rState === rSend)
+  io.r.bits := NastiReadDataChannel(
+    id = rId,
+    data = rData(nastiXDataBits - 1, 0),
+    last = rLen === UInt(0) && rCount === UInt(0))
+
+  io.aw.ready := Bool(false)
+  io.w.ready := Bool(false)
+  io.b.valid := Bool(false)
+
+  require(w % nastiXDataBits === 0)
+
+  assert(!io.ar.valid ||
+    (io.ar.bits.addr(log2Ceil(w/8)-1, 0) === UInt(0) &&
+     io.ar.bits.len(log2Ceil(w/nastiXDataBits)-1, 0).andR &&
+     io.ar.bits.size === UInt(log2Up(nastiXDataBits/8))),
+   "Invalid read request")
+}
